968269b33745bc23abc041e1641aeba73749fda0
Juan Carlos <jcervanteslivon@gmail.com>
2024-11-13 19:30:48 -0500

    Primera versión del proyecto integrador

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  jcerv_pm92n0w
+ * Created: Oct 28, 2024
+ */
+
+insert into alumnos(nombres, apellidos) values ('Sergio', 'Fernandez');
+insert into alumnos(nombres, apellidos) values ('Dajana', 'Damian');
+
